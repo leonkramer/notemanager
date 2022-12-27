693cac0444f17a918c92d3ba1d48b1f63a9a6bf0
lkramer <leonkramer@gmail.com>
2022-12-27 18:26:46 +0100

    added win64 build support

--- path_windows.go
+++ path_windows.go
@@ -1,1 +1,53 @@
+// +build windows
+
 package main
+
+import (
+	"os"
+	_"fmt"
+	"log"
+	"github.com/gosimple/conf"
+)
+
+func parseConfig() (c Config) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	cfg, err := conf.ReadFile(homedir + `/AppData/Roaming/Notemanager/noterc`)
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	datadir, err := cfg.String("default", "datadir")
+	if err == nil {
+		c.DataDir = datadir
+	} else {
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.DataDir = homedir + `/AppData/Roaming/Notemanager`
+	}
+	c.TemplateDir = c.DataDir + `/templates`
+	c.TempDir = c.DataDir + `/tmp`
+	c.NoteDir = c.DataDir + `/notes`
+	c.VersionTimeFormat = "20060102-150405"
+	c.OutputTimeFormatShort = "2006-01-02"
+	c.OutputTimeFormatLong = "2006-01-02 15:04:05"
+
+	c.Editor, err = cfg.String("default", "editor")
+	// Default to notepad.exe as editor
+	if err != nil {
+		c.Editor = `notepad`
+	}
+
+	// Default to more.exe as reader with pagination
+	c.TerminalReader, err = cfg.String("default", "terminalReader")
+	if err != nil {
+		c.TerminalReader = `more`
+	}
+
+	return
+}
